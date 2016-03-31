d44eb41ba7cab1482b91db16da544395ce2b6432
Jia Sui <jsfaint@gmail.com>
2016-03-31 10:48:14 +0800

    Avoid redefine TRUE/FALSE

--- term.h
+++ term.h
@@ -1,8 +1,12 @@
 #ifndef __TERM_H__
 #define __TERM_H__
 
+#ifndef TRUE
 #define TRUE        1
+#endif
+#ifndef FALSE
 #define FALSE       0
+#endif
 
 void tc_set_baudrate(int fd, int speed);
 int tc_set_port(int fd, int databits, int stopbits, int parity);
