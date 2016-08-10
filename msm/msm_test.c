5afdf224421a1a9c142bb10af71347c3381406bd
shengkui.leng <shengkui.leng@advantech.com.cn>
2016-08-10 17:22:54 +0800

    Modify function to read data from side A/B and verify it

--- msm/msm_test.c
+++ msm/msm_test.c
@@ -19,10 +19,13 @@
 #include <fcntl.h>
 #include <signal.h>
 #include "msm_test.h"
+#include "adv_spi.h"
+#include "term.h"
 
 void msm_print_status();
 void msm_print_result(int fd);
 void *msm_test(void *args);
+static int open_port(void);
 
 
 test_mod_t test_mod_msm = {
@@ -35,26 +38,27 @@ test_mod_t test_mod_msm = {
     .print_result = msm_print_result
 };
 
+#define DEVICE_NAME     "/dev/ttyS1"
 
 #define ADVSPI_DEVICE   "/dev/advspi3962"
 
-#define PACKET_SIZE     15
+/* Data size if 64KB(Half size of nvSRAM) */
+#define PACKET_SIZE     HALF_SIZE
 
-/* Data pattern sent by machine A and B. */
-char data_a[PACKET_SIZE+1] = "123456789ABCDEF\0";
-char data_b[PACKET_SIZE+1] = "FEDCBA987654321\0";
+/* Data pattern to write. */
+char data_55[PACKET_SIZE];
+char data_aa[PACKET_SIZE];
 
 /* Counter of test times, successful and failed test. */
 unsigned long counter_test = 0;
 unsigned long counter_success = 0;
 unsigned long counter_fail = 0;
 
-/* The flag to inform the program exit from the loop */
-int exit_flag = 0;
 
-int write_data(int fd, int addr, char *buf);
-int read_data(int fd, int addr, char *cmp_buf);
-void print_result(void);
+static int read_data_a(int fd, int addr, char *cmp_buf);
+static int read_data_b(int fd, int addr, char *cmp_buf);
+static void print_result(void);
+static int open_port(void);
 
 
 void msm_print_status()
@@ -92,38 +96,36 @@ void *msm_test(void *args)
     }
     log_print(log_fd, "open storage device is Successful!\n");
 
+    memset(data_55, 0x55, sizeof(data_55));
+    memset(data_aa, 0xAA, sizeof(data_aa));
 
     if (g_machine == 'A') {
         while (g_running) {
-            if (exit_flag) {
-                break;
-            }
-
             counter_test++;
 
             /* Write data */
-            bytes = write_data(spi, addr, data_a);
-            if (exit_flag) {
+            bytes = advspi_write_a(spi, data_55, PACKET_SIZE, 0);
+            if (!g_running) {
                 break;
             }
             if (bytes != PACKET_SIZE) {
-                printf("write %d bytes: %-15s    FALSE!\n", bytes, " ");
+                printf("write %d bytes(%02X)   FALSE!\n", bytes, data_55[0]);
                 counter_fail++;
                 continue;
             } else {
-                printf("write %d bytes: %-15s    OK!\n", bytes, data_a);
+                printf("write %d bytes(%02X)   OK!\n", bytes, data_55[0]);
             }
             sleep(10);
 
-            bytes = read_data(spi, addr, data_b);
-            if (exit_flag) {
+            bytes = read_data_b(spi, addr, data_aa);
+            if (!g_running) {
                 break;
             }
             if (bytes != PACKET_SIZE) {
-                printf("read %d bytes: %-15s     FALSE!\n", bytes, " ");
+                printf("read %d bytes(%02X)   FALSE!\n", bytes, data_aa[0]);
                 counter_fail++;
             } else {
-                printf("read %d bytes: %-15s     OK!\n", bytes, data_b);
+                printf("read %d bytes(%02X)   OK!\n", bytes, data_aa[0]);
                 counter_success++;
             }
             sleep(2);
@@ -131,36 +133,32 @@ void *msm_test(void *args)
     } else {
         sleep(10);
         while (g_running) {
-            if (exit_flag) {
-                break;
-            }
-
             counter_test++;
 
-            bytes = read_data(spi, addr, data_a);
-            if (exit_flag) {
+            bytes = read_data_a(spi, addr, data_55);
+            if (!g_running) {
                 break;
             }
             if (bytes != PACKET_SIZE) {
-                printf("read %d bytes: %-15s     FALSE!\n", bytes, " ");
+                printf("read %d bytes(%02X)   FALSE!\n", bytes, data_55[0]);
                 counter_fail++;
                 continue;
             } else {
-                printf("read %d bytes: %-15s     OK!\n", bytes, data_a);
+                printf("read %d bytes(%02X)   OK!\n", bytes, data_55[0]);
                 counter_success++;
             }
             sleep(2);
 
-            bytes = write_data(spi, addr, data_b);
-            if (exit_flag) {
+            bytes = advspi_write_b(spi, data_aa, PACKET_SIZE, 0);
+            if (!g_running) {
                 break;
             }
             if (bytes != PACKET_SIZE) {
-                printf("write %d bytes: %-15s    FALSE!\n", bytes, " ");
+                printf("write %d bytes(%02X)   FALSE!\n", bytes, data_aa[0]);
                 counter_fail++;
                 continue;
             } else {
-                printf("write %d bytes: %-15s    OK!\n", bytes, data_b);
+                printf("write %d bytes(%02X)   OK!\n", bytes, data_aa[0]);
             }
             sleep(10);
         }
@@ -176,55 +174,12 @@ void *msm_test(void *args)
 }
 
 
-
-
 /******************************************************************************
  * NAME:
- *      write_data
+ *      read_data_a
  *
  * DESCRIPTION:
- *      Write data to storage adapter.
- *
- * PARAMETERS:
- *      fd   - The fd of serial port
- *      addr - The offset to start write
- *      buf  - The buffer of data
- *
- * RETURN:
- *      Number of bytes wrote
- ******************************************************************************/
-int write_data(int fd, int addr, char *buf)
-{
-    int bytes = 0;
-    int ret = 0;
-    int len = PACKET_SIZE;
-
-    lseek(fd, addr, SEEK_SET);
-    while (len > 0) {
-        if (exit_flag) {
-            break;
-        }
-
-        ret = write(fd, buf, len);
-        if (ret <= 0) {
-            //sleep(1);
-            continue;
-        }
-
-        bytes += ret;
-        len = len - ret;
-    }
-
-    return bytes;
-}
-
-
-/******************************************************************************
- * NAME:
- *      read_data
- *
- * DESCRIPTION:
- *      Read data from storage adapter, and compare it with predefined pattern.
+ *      Read data from storage zone A, and compare it with predefined pattern.
  *
  * PARAMETERS:
  *      fd       - The fd of serial port
@@ -234,47 +189,64 @@ int write_data(int fd, int addr, char *buf)
  * RETURN:
  *      Number of bytes readed
  ******************************************************************************/
-int read_data(int fd, int addr, char *cmp_buf)
+static int read_data_a(int fd, int addr, char *cmp_buf)
 {
     int ret = 0;
-    int bytes = 0;
     int len = PACKET_SIZE;
     char buf[PACKET_SIZE];
 
     memset(buf, 0, sizeof(buf));
 
-    while (!exit_flag) {
-        bytes = 0;
-        len = PACKET_SIZE;
-
-        lseek(fd, addr, SEEK_SET);
-
-        while (len > 0) {
-            if (exit_flag) {
-                break;
-            }
-
-            ret = read(fd, buf, len);
-            if (ret <= 0) {
-                sleep(1);
-                continue;
-            }
-
-            bytes += ret;
-            len = len - ret;
-        }
-
-        if (memcmp(cmp_buf, buf, PACKET_SIZE) == 0) {
-            break;
-        }
-        sleep(5);
+    ret = advspi_read_a(fd, buf, len, 0);
+    if (ret <= 0) {
+        return ret;
     }
 
-    return bytes;
+    if (memcmp(cmp_buf, buf, len) == 0) {
+        return -1;
+    }
+
+    return ret;
 }
 
 
-void print_result(void)
+/******************************************************************************
+ * NAME:
+ *      read_data_b
+ *
+ * DESCRIPTION:
+ *      Read data from storage zone B, and compare it with predefined pattern.
+ *
+ * PARAMETERS:
+ *      fd       - The fd of serial port
+ *      addr     - The offset to start read
+ *      cmp_buf  - The data to compare with the readed data
+ *
+ * RETURN:
+ *      Number of bytes readed
+ ******************************************************************************/
+static int read_data_b(int fd, int addr, char *cmp_buf)
+{
+    int ret = 0;
+    int len = PACKET_SIZE;
+    char buf[PACKET_SIZE];
+
+    memset(buf, 0, sizeof(buf));
+
+    ret = advspi_read_b(fd, buf, len, 0);
+    if (ret <= 0) {
+        return ret;
+    }
+
+    if (memcmp(cmp_buf, buf, len) == 0) {
+        return -1;
+    }
+
+    return ret;
+}
+
+
+static void print_result(void)
 {
     printf("\n");
     if (counter_fail > 0) {
@@ -285,4 +257,32 @@ void print_result(void)
         printf("Test OK. Test time:%lu\n", counter_test);
         printf("PASS\n");
     }
+}
+
+
+/* Open and setup serial port */
+static int open_port(void)
+{
+    int fd;
+    char *dev = DEVICE_NAME;
+    int baud = 115200;
+    int databits = 8;
+    int parity = 0;
+    int stopbits = 1;
+
+    fd = tc_init(dev);
+    if (fd == -1) {
+        return -1;
+    }
+
+    /* Set baudrate */
+    tc_set_baudrate(fd, baud);
+
+    /* Set databits, stopbits, parity ... */
+    if (tc_set_port(fd, databits, stopbits, parity) == -1) {
+        tc_deinit(fd);
+        return -1;
+    }
+
+    return fd;
 }
