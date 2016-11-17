4d3dc6f7d1749e285dc0fa97b303f5783e700184
Jia Sui <jia.sui@advantech.com.cn>
2016-11-17 16:05:31 +0800

    [hsm] Modify result string

--- hsm/hsm_test.c
+++ hsm/hsm_test.c
@@ -80,9 +80,9 @@ static void hsm_print_status()
 static void hsm_print_result(int fd)
 {
     if (test_mod_hsm.pass) {
-        write_file(fd, "HSM: PASS, test=%lu\n", test_counter);
+        write_file(fd, "HSM: PASS. test=%lu\n", test_counter);
     } else {
-        write_file(fd, "HSM: FAIL, test=%lu, switch fail=%lu, hold fail=%lu\n",
+        write_file(fd, "HSM: FAIL. test=%lu, switch fail=%lu, hold fail=%lu\n",
                 test_counter, switch_fail_cntr, hold_fail_cntr);
     }
 }
