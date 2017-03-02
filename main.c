88618cd720d8725a7b6dafe07eea40c210770f64
Jia Sui <jia.sui@advantech.com.cn>
2017-03-02 14:54:39 +0800

    Raise version number

--- main.c
+++ main.c
@@ -26,7 +26,7 @@
 #include "main.h"
 
 /* Version of the program */
-#define PROGRAM_VERSION     "0.08"
+#define PROGRAM_VERSION     "0.10"
 
 static int g_runned_minute = 0;
 static int g_runned_second = 0;
