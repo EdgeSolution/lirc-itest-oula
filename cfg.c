549b5b88dd799958c9667e9f3352972752311010
Jia Sui <jsfaint@gmail.com>
2019-01-04 10:32:19 +0800

    Check if HSM switch test duration is larger than total test duration

--- cfg.c
+++ cfg.c
@@ -682,6 +682,15 @@ static int input_hsm_loop(uint64_t *loop)
             continue;
         }
 
+        //Compare HSM switch time with test duration
+        if (tmp*(WAIT_IN_MS*2/1000) >= g_duration*60) {
+            printf("Invalid HSM test loop, HSM switch duration (%lus)"
+                    " must smaller than total test duration (%lus)\n",
+                    tmp*(WAIT_IN_MS*2/1000), g_duration*60);
+            ret = -1;
+            continue;
+        }
+
         break;
     } while (ret == -1);
 
